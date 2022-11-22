add7c03df2d6a13b2082dc04d2d86bde9a8efa94
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-11-22 14:34:52 +0700

    api

--- BlogRestApi/src/Controller/BlogController.php
+++ BlogRestApi/src/Controller/BlogController.php
@@ -30,22 +30,50 @@ class BlogController extends AbstractController
         $json = $serializer->serialize($blogs, 'json');
         $xml = $serializer->serialize($blogs, 'xml');
         //Bước 3: trả về API
-        return new Response($json, Response::HTTP_OK,
+        return new Response(
+            $json,
+            Response::HTTP_OK,
             [
                 'content-type' => 'application/json',
-               // 'IDE' => 'VS Code'
+                'Access-Control-Allow-Origin' => '*' //enable CORS
+                // 'IDE' => 'VS Code'
+            ]
+        );
+    }
+
+    //SQL: SELECT * FROM Blog WHERE id = '$id'
+    #[Route('/{id}', methods: 'GET', name: 'view_blog_by_id')]
+    public function viewBlogById($id, SerializerInterface $serializer)
+    {
+        $blog = $this->blogRepository->find($id);
+        //check xem id có tồn tại trong DB không => $blog có null hay không
+        if ($blog == null) {
+            $error = "<center><h1 style='color:red;'><i>Blog not found !</i></h1></center>";
+            return new Response(
+                $error,
+                Response::HTTP_NOT_FOUND,
+                [
+                    'content-type' => 'text/html'
+                ]
+            );
+        }
+        //nếu có dữ liệu thì convert thành json và trả về response tương ứng
+        $json = $serializer->serialize($blog, 'json');
+        return new Response(
+            $json,
+            Response::HTTP_OK,
+            [
+                'content-type' => 'application/json'
             ]
         );
     }
 
 
 
-    
     //Note: có thể set cùng 1 route url cho nhiều function khác nhau
     //với điều kiện là method khác nhau
     #[Route('/', methods: ['POST'], name: 'add_new_blog')]
     public function createNewBlog()
     {
-        
     }
 }
