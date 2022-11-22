ed28de8ed44807e51731ea190c818c6a4c15def5
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-11-22 13:53:17 +0700

    api

--- BlogRestApi/src/Controller/BlogController.php
+++ BlogRestApi/src/Controller/BlogController.php
@@ -12,32 +12,35 @@ use Symfony\Component\Serializer\SerializerInterface;
 class BlogController extends AbstractController
 {
     //Cách 1: khai báo SerializerInterface sử dụng constructor
-    private $serializer1;
+    private $blogRepository;
 
-    public function __construct(SerializerInterface $serializerInterface)
+    public function __construct(BlogRepository $blogRepository)
     {
-        $this->serializer1 = $serializerInterface;
+        $this->blogRepository = $blogRepository;
     }
 
     //SQL: SELECT * FROM Blog
-    #[Route('/', methods: ['GET'], name: 'view_all_blog')]
+    #[Route('/', methods: 'GET', name: 'view_all_blog')]
     //Cách 2: khai báo SerializerInterface sử dụng parameter
-    public function viewBlogList(SerializerInterface $serializer2, BlogRepository $blogRepository)
+    public function viewBlogList(SerializerInterface $serializer)
     {
         //Bước 1: lấy dữ liệu từ bảng Blog trong DB và lưu vào array
-        $blogs = $blogRepository->findAll();
+        $blogs = $this->blogRepository->findAll();
         //Bước 2: chuyển đổi dữ liệu sang API (JSON hoặc XML) sử dụng SerializerInterface
-        $json = $this->serializer1->serialize($blogs, 'json');
-        $xml = $serializer2->serialize($blogs, 'xml');
+        $json = $serializer->serialize($blogs, 'json');
+        $xml = $serializer->serialize($blogs, 'xml');
         //Bước 3: trả về API
-        return new Response($json, 404,
+        return new Response($json, Response::HTTP_OK,
             [
-                'content-type' => 'application/json'
+                'content-type' => 'application/json',
+               // 'IDE' => 'VS Code'
             ]
         );
-        //200: HTTP_OK
     }
 
+
+
+    
     //Note: có thể set cùng 1 route url cho nhiều function khác nhau
     //với điều kiện là method khác nhau
     #[Route('/', methods: ['POST'], name: 'add_new_blog')]
