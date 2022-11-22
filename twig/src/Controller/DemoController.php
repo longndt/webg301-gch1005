f761f13c714f948837957f9207e391fca657c53f
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-11-22 17:34:25 +0700

    twig

--- twig/src/Controller/DemoController.php
+++ twig/src/Controller/DemoController.php
@@ -11,8 +11,37 @@ class DemoController extends AbstractController
     #[Route('/demo', name: 'app_demo')]
     public function index(): Response
     {
-        return $this->render('demo/index.html.twig', [
-            'controller_name' => 'DemoController',
+        return $this->render('demo/index.html.twig');
+    }
+
+    #[Route('/')]
+    public function demo()
+    {
+        $school = "University of Greenwich (Vietnam)";
+        $subject = "Web Project";
+        $semester = "Fall 2022";
+        $quantity = 40;
+        return $this->render(
+            'demo/demo.html.twig',
+            [
+                'school' => $school,
+                'subject' => $subject,
+                'hocky' => $semester,
+                'framework' => 'Symfony',
+                'quantity' => $quantity,
+                'point' => 8.5,
+                'grade' => 'M',
+                'pass' => true
+            ]
+        );
+    }
+
+    #[Route('/greenwich')]
+    public function greenwich() {
+        $students = array('Nam', 'Minh', 'Huong', 'Tuan', 'Cuong','Hung','Loan');
+        return $this->render('demo/students.html.twig',
+        [
+            'students' => $students
         ]);
     }
 }
