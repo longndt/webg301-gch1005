9bc0d3549c9c307b226eb914e71fbc17b45642fb
Nguyen Dinh Tran Long <longndt226@gmail.com>
2022-11-29 17:37:22 +0700

    todo

---
+++ todo/src/Controller/TodoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TodoRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TodoController extends AbstractController
+{
+    #[Route('/', name: 'todo_index')]
+    public function viewTodoList(TodoRepository $todoRepository)
+    {
+        $todos = $todoRepository->findAll();
+        return $this->render('todo/index.html.twig', 
+        [
+             'todos' => $todos
+        ]);
+    }
+}
